5afa6a6509b7b629e6557993eee8b102c63c6325
Adão Oliveira <adao.domingos@ituran.com.br>
2020-07-24 00:37:54 -0300

    Praticando Js com o DarkMode

--- darkmode.js
+++ darkmode.js
@@ -1,15 +1,70 @@
-var options = {
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
-  label: '🌓', // default: ''
+const html = document.querySelector("html")
+const checkbox = document.querySelector("input[name=theme]")
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+
+const initialColors = {
+    bg: getStyle(html, "--bg"),
+    bgPanel: getStyle(html, "--bg-panel"),
+    colorHeadings: getStyle(html, "--color-headings"),
+    colorText: getStyle(html, "--color-text"),
+    colorball: getStyle(html, "--color-ball"),
 }
 
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
+const darkMode = {
+    bg: "#333333",
+    bgPanel: "#434343",
+    colorHeadings: "#3664FF",
+    colorText: "#B5B5B5",
+    colorball: "#ff0000"
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+})
+
+let curs = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+  curs.style.left = (x - 15) + 'px';
+  curs.style.top = (y - 15) + 'px';
+});
+
+document.addEventListener('click', (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+  
+  curs.classList.add('explosion');
+  setTimeout(function() {
+    curs.classList.remove('explosion');
+  }, 900);
+  
+  let blackhole = document.createElement('blackhole');
+  blackhole.style.left = x + 'px';
+  blackhole.style.top = y + 'px';
+  document.body.appendChild(blackhole);
+  let size = Math.random() * 40;
+  blackhole.style.width = 1 + size + 'px';
+  blackhole.style.height = 1 + size + 'px';
+  
+  setTimeout(function() {
+    blackhole.remove();
+  }, 2000);
+});
